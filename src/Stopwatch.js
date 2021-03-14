5fe218e2719c86e63bce2cbc69f39796404ab673
Marc Denning <marc.denning@gmail.com>
2021-03-14 19:22:53 -0400

    Add and delete stopwatches.

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,18 +1,22 @@
+import './Stopwatch.css';
 import formatDuration from './formatDuration';
 
 export default function Stopwatch(props) {
-  const {stopwatch, onToggle} = props;
+  const {stopwatch, onToggle, onSelect} = props;
 
   return (
-    <div>
-      <span><input type="checkbox" checked={stopwatch.isSelected}/></span>
-      <span>{formatDuration(stopwatch.elapsedTime)}</span>
-      <span>{stopwatch.label}</span>
-      <span>
+    <div className={'stopwatch-wrapper'}>
+      <span className={'stopwatch-selector'}>
+        <input type="checkbox" checked={stopwatch.isSelected}
+               onChange={(e) => onSelect(e, stopwatch)}/>
+      </span>
+      <span className={'stopwatch-duration'}>{formatDuration(stopwatch.elapsedTime)}</span>
+      <span className={'stopwatch-label'}>{stopwatch.label}</span>
+      <span className={'stopwatch-toggle-button-wrapper'}>
         <button type="button"
                 onClick={(e) => onToggle(e, stopwatch)}>{stopwatch.isPaused ? 'Resume' : 'Pause'}
         </button>
       </span>
     </div>
   );
-}
+};
--- src/StopwatchContainer.js
+++ src/StopwatchContainer.js
@@ -14,25 +14,76 @@ export default class StopwatchContainer extends Component {
         isSelected: false
       }]
     };
-    this.updateStopwatchState = this.updateStopwatchState.bind(this);
+    this.addStopwatch = this.addStopwatch.bind(this);
+    this.deleteStopwatches = this.deleteStopwatches.bind(this);
+    this.containerAction = this.containerAction.bind(this);
+    this.selectStopwatch = this.selectStopwatch.bind(this);
     this.tick = this.tick.bind(this);
     this.toggleStopwatch = this.toggleStopwatch.bind(this);
+    this.updateStopwatchState = this.updateStopwatchState.bind(this);
   }
 
-  updateStopwatchState(updatedStopwatch) {
-    const targetIndex = this.state.stopwatches.findIndex((s) => s.id === updatedStopwatch.id);
-
+  addStopwatch() {
     this.setState({
       stopwatches: [
-        ...this.state.stopwatches.slice(0, targetIndex),
-        updatedStopwatch,
-        ...this.state.stopwatches.slice(targetIndex + 1, this.state.stopwatches.length)
+        ...this.state.stopwatches,
+        {
+          id: Math.max(...this.state.stopwatches.map((s) => s.id)) + 1,
+          elapsedTime: 0,
+          label: 'Stopwatch',
+          isPaused: true,
+          isSelected: false,
+          timeoutId: null
+        }
       ]
     });
   }
 
+  containerAction() {
+    const selectedStopwatches = this.state.stopwatches.filter((s) => s.isSelected);
+
+    if (selectedStopwatches.length === 0) {
+      this.addStopwatch();
+    } else {
+      this.deleteStopwatches(selectedStopwatches);
+    }
+  }
+
+  deleteStopwatches(stopwatches) {
+    const stopwatchIds = stopwatches.map((s) => s.id);
+
+    for (const stopwatch of stopwatches) {
+      if (stopwatch.timeoutId !== null) {
+        clearInterval(stopwatch.timeoutId);
+      }
+    }
+
+    this.setState({
+      stopwatches: [
+        ...this.state.stopwatches.filter((s) => stopwatchIds.indexOf(s.id) === -1)
+      ]
+    });
+  }
+
+  getContainerButtonText(stopwatches) {
+    if (stopwatches.findIndex((s) => s.isSelected) === -1) {
+      return 'Add';
+    }
+    return 'Delete';
+  }
+
+  selectStopwatch(event, stopwatch) {
+    const updatedStopwatch = {
+      ...stopwatch,
+      isSelected: !stopwatch.isSelected
+    };
+
+    this.updateStopwatchState(updatedStopwatch);
+  }
+
   tick(stopwatchId) {
     const stopwatch = this.state.stopwatches.find((s) => s.id === stopwatchId);
+
     const updatedStopwatch = {
       ...stopwatch,
       elapsedTime: stopwatch.elapsedTime += this.interval
@@ -56,15 +107,27 @@ export default class StopwatchContainer extends Component {
     this.updateStopwatchState(updatedStopwatch);
   }
 
+  updateStopwatchState(updatedStopwatch) {
+    const targetIndex = this.state.stopwatches.findIndex((s) => s.id === updatedStopwatch.id);
+
+    this.setState({
+      stopwatches: [
+        ...this.state.stopwatches.slice(0, targetIndex),
+        updatedStopwatch,
+        ...this.state.stopwatches.slice(targetIndex + 1, this.state.stopwatches.length)
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="app-container">
         <div className="app-body">
           {this.state.stopwatches.map(stopwatch => (
-            <Stopwatch key={stopwatch.id} stopwatch={stopwatch} onToggle={this.toggleStopwatch}/>))}
+            <Stopwatch key={stopwatch.id} stopwatch={stopwatch} onToggle={this.toggleStopwatch} onSelect={this.selectStopwatch}/>))}
         </div>
         <footer className="app-footer">
-          <button type="button" className="button-primary">Add</button>
+          <button type="button" className={'button-primary'} onClick={this.containerAction}>{this.getContainerButtonText(this.state.stopwatches)}</button>
         </footer>
       </div>
     );
