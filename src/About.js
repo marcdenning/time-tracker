af267ca66ce73b60309c5200e18c09d0feca09af
Marc Denning <marc.denning@gmail.com>
2021-03-14 20:09:50 -0400

    Add about page.

--- src/About.js
+++ src/About.js
@@ -1,16 +1,23 @@
-function About() {
+export default function About() {
   return (
     <div>
       <h2>Background</h2>
-      <p>Why I built this app.</p>
+      <p>As a consultant, I have to track the time I am working on client projects.
+        As a practice manager, other non-billable responsibilities also take time, and I would like to better track the
+        effort I am applying to those activities.
+        For instance, how much time do I spend working on proposals? Or internal initiatives?</p>
 
-      <h2>Privacy Policy</h2>
-      <p>What data we collect and use it for.</p>
+      <p>At the time of writing, I've been away from React and other front-end web technologies for a bit, so I decided
+        to roll my own time tracking app.
+        I know there are plenty of free and commercial solutions out there, but I enjoy the process of building and
+        creating.</p>
 
-      <h2>Terms &amp; Conditions</h2>
-      <p>What your rights are as a user of the app.</p>
+      <p>I plan to publish updates to the app over time as I expand on its features.
+        I expect to only build features that I am going to use and aim to keep this simple.
+        That said, if you have ideas or want to use this for yourself, feel free to submit a GitHub issue or pull request.</p>
+
+      <a href="https://www.iubenda.com/privacy-policy/73184110/legal" className="iubenda-white iubenda-embed"
+         title="Privacy Policy" target="_blank">Privacy Policy</a>
     </div>
   );
 }
-
-export default About;
