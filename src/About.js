34fde4ec173d50e734520da81329fa1a8bc09132
Marc Denning <marc.denning@gmail.com>
2023-02-11 16:34:57 -0500

    Update About and icons.

--- src/About.js
+++ src/About.js
@@ -2,19 +2,23 @@ export default function About() {
   return (
     <div>
       <h2>Background</h2>
-      <p>As a consultant, I have to track the time I am working on client projects.
-        As a practice manager, other non-billable responsibilities also take time, and I would like to better track the
-        effort I am applying to those activities.
-        For instance, how much time do I spend working on proposals? Or internal initiatives?</p>
-
-      <p>At the time of writing, I've been away from React and other front-end web technologies for a bit, so I decided
-        to roll my own time tracking app.
-        I know there are plenty of free and commercial solutions out there, but I enjoy the process of building and
-        creating.</p>
+      <p>This time tracker is intended to help track time spent on multiple projects or tasks at one time.
+        I am using to help myself keep track of tasks at work, and to exercise my front-end development skills a bit.</p>
 
       <p>I plan to publish updates to the app over time as I expand on its features.
         I expect to only build features that I am going to use and aim to keep this simple.
         That said, if you have ideas or want to use this for yourself, feel free to submit a GitHub issue or pull request.</p>
+
+      <h2>Instructions</h2>
+
+      <ul>
+        <li>Use the add button to add another stopwatch to track another task.</li>
+        <li>Individual stopwatches can be started and stopped in parallel.</li>
+        <li>Select a stopwatch with the checkbox to delete it.</li>
+        <li>Multiple stopwatches may be deleted simultaneously.</li>
+        <li>Use the reset button to set all stopwatches to zero.</li>
+        <li>The elapsed time on a stopwatch may be modified <em>only</em> when the stopwatch is paused.</li>
+      </ul>
 
       <a href="https://www.iubenda.com/privacy-policy/73184110/legal" className="iubenda-white iubenda-embed"
          title="Privacy Policy" target="_blank" rel="noreferrer">Privacy Policy</a>
